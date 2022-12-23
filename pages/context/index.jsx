f5420e68cba3c389c23d6326a8ebd6576b79f606
Medidanagaraju <nagarajumedida123@gmail.com>
2022-12-23 15:09:46 +0530

    123

--- pages/context/index.jsx
+++ pages/context/index.jsx
@@ -4,19 +4,17 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-
-// const socket = io('https://warm-wildwood-81069.herokuapp.com');
-
 const ContextProvider = ({ children }) => {
-const socket = io('https://kartik.onrender.com/');
-// const socket = io('http://localhost:9000');
 
-    const [callAccepted, setCallAccepted] = useState(false);
-    const [callEnded, setCallEnded] = useState(false);
-    const [stream, setStream] = useState();
+    const socket = io('https://kartik.onrender.com/');
+    // const socket = io('http://localhost:9000');
     const [name, setName] = useState('');
     const [call, setCall] = useState({});
     const [me, setMe] = useState('');
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
+    const [stream, setStream] = useState();
+
 
     const myVideo = useRef();
     const userVideo = useRef();
@@ -32,17 +30,10 @@ const socket = io('https://kartik.onrender.com/');
                     myVideo.current.srcObject = currentStream
                     // console.log(myVideo.current.srcObject)
                 }
-
-                // myVideo.current.srcObject= currentStream;
-
             });
 
         socket.on('ime', (id) => setMe(id));
-
-
-
         socket.on('callUser', ({ from, name: callerName, signal }) => {
-            
             setCall({ isReceivingCall: true, from, name: callerName, signal });
         });
     }, []);
@@ -59,14 +50,12 @@ const socket = io('https://kartik.onrender.com/');
 
         peer.on('stream', (currentStream) => {
             // userVideo.current.srcObject = currentStream;
-            if(userVideo.current){
-           
+            if (userVideo.current) {
 
                 userVideo.current.srcObject = currentStream;
                 // console.log(userVideo.current.srcObject)
             }
-            
-            
+
         });
 
         peer.signal(call.signal);
@@ -75,25 +64,25 @@ const socket = io('https://kartik.onrender.com/');
     };
 
     const callUser = (id) => {
-   
+
         const peer = new Peer({ initiator: true, trickle: false, stream });
-      
+
         peer.on('signal', (data) => {
-            
+
             socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
         });
 
         peer.on('stream', (currentStream) => {
-            
+
             userVideo.current.srcObject = currentStream;
-           
+
         });
 
         socket.on('callAccepted', (signal) => {
             setCallAccepted(true);
 
             peer.signal(signal);
-        }); 
+        });
 
         connectionRef.current = peer;
     };
